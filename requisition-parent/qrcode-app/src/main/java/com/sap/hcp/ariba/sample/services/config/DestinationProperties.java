33e6748640f37be448b88cca9183d79a351e2d90
Tsvetislav Viktorov <tsvetislav.viktorov@sap.com>
2017-01-20 17:21:43 +0200

    Lookup connectivity configuration for each new DestinationUtils instance
    Change-Id: I56a4b76ee2793c6b694b4c73e1bcff30aeb9fe1c

--- requisition-parent/qrcode-app/src/main/java/com/sap/hcp/ariba/sample/services/config/DestinationProperties.java
+++ requisition-parent/qrcode-app/src/main/java/com/sap/hcp/ariba/sample/services/config/DestinationProperties.java
@@ -5,33 +5,31 @@ public class DestinationProperties {
 	private static final String COMMON_COMMODITY_CODE_DOMAIN = "CommonCommodityCodeDomain";
 	private static final String COMMON_COMMODITY_CODE_NAME = "CommonCommodityCodeName";
 	private static final String NAMESPACE_XMLN_SVARIANT = "namespaceXMLNSvariant";
-	private static final String REQUISITION_NAME= "requisitionName";
-	private static final String REQUISITION_COMMENT= "requisitionComment";
-	private static final String URL= "URL";
-	private static final String PASSWORD= "Password";
-	private static final String USER= "User";
-	private static final String CURRENCY= "currency";
-	private static final String COMMODITY_CODE= "commodityCode";
-	private static final String HEADER_UNIQUE_NAME= "headerUniqueName";
-	private static final String PASSWORD_ADAPTER= "passwordAdapter";
-	private static final String ORIGINATING_SYSTEM_ID= "originatingSystemId";
-	private static final String ORIGINATING_SYSTEM= "originatingSystem";
-	private static final String DELIVER_TO= "deliverTo";
-	private static final String BUSINESS_UNIT= "businessUnit";
-	private static final String SHIP_TO= "shipTo";
-	private static final String REQUESTER= "requester";
-	private static final String PREPARER= "preparer";
-	private static final String BILLING_ADDRESS= "billingAddress";
-	private static final String PART_NUMBER_ITEM1= "partNumberItem1";
-	private static final String PART_NUMBER_ITEM2= "partNumberItem2";
-	private static final String PART_NUMBER_ITEM3= "partNumberItem3";
+	private static final String REQUISITION_NAME = "requisitionName";
+	private static final String REQUISITION_COMMENT = "requisitionComment";
+	private static final String URL = "URL";
+	private static final String PASSWORD = "Password";
+	private static final String USER = "User";
+	private static final String CURRENCY = "currency";
+	private static final String COMMODITY_CODE = "commodityCode";
+	private static final String HEADER_UNIQUE_NAME = "headerUniqueName";
+	private static final String PASSWORD_ADAPTER = "passwordAdapter";
+	private static final String ORIGINATING_SYSTEM_ID = "originatingSystemId";
+	private static final String ORIGINATING_SYSTEM = "originatingSystem";
+	private static final String DELIVER_TO = "deliverTo";
+	private static final String BUSINESS_UNIT = "businessUnit";
+	private static final String SHIP_TO = "shipTo";
+	private static final String REQUESTER = "requester";
+	private static final String PREPARER = "preparer";
+	private static final String BILLING_ADDRESS = "billingAddress";
+	private static final String PART_NUMBER_ITEM1 = "partNumberItem1";
+	private static final String PART_NUMBER_ITEM2 = "partNumberItem2";
+	private static final String PART_NUMBER_ITEM3 = "partNumberItem3";
 
 	private static DestinationUtils connectivityConfiguration;
 
 	public DestinationProperties() {
-		if (connectivityConfiguration == null) {
-			connectivityConfiguration = new DestinationUtils();
-		}
+		connectivityConfiguration = new DestinationUtils();
 	}
 
 	public String getRequisitionName() {
